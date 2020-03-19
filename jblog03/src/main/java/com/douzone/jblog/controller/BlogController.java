ef00aeb34eb3479eefb80f4bd09cf517d4f3ddcd
haeyonghahn <youg1322@naver.com>
2020-03-19 16:34:17 +0900

    error 페이지 수정

--- jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
@@ -32,143 +32,140 @@ public class BlogController {
 	private CategoryService categoryService;
 	@Autowired
 	private PostService postService;
-	
-	@RequestMapping({"", "/{pathNo1}", "/{pathNo1}/{pathNo2}"})
-	public String main(@PathVariable String id
-			, @PathVariable Optional<Long> pathNo1
-			, @PathVariable Optional<Long> pathNo2
-			, Model model) {
-		
+
+	@RequestMapping({ "", "/{pathNo1}", "/{pathNo1}/{pathNo2}" })
+	public String main(@PathVariable String id, @PathVariable Optional<Long> pathNo1,
+			@PathVariable Optional<Long> pathNo2, Model model) {
+
 		Long categoryNo = 0L;
 		Long postNo = 0L;
-		
-		System.out.println(categoryNo);
-		System.out.println(postNo);
-		
-		if(pathNo2.isPresent()) {
+
+		BlogVo blogVo = blogService.getBlog(id);
+
+		if (blogVo == null) {
+			return "/WEB-INF/views/error/404.jsp";
+		}
+
+		if (pathNo2.isPresent()) {
 			postNo = pathNo2.get();
 			categoryNo = pathNo1.get();
-		} else if(pathNo1.isPresent()) {
+		} else if (pathNo1.isPresent()) {
 			categoryNo = pathNo1.get();
 			postNo = postService.getPostNo(categoryNo);
 		} else {
 			categoryNo = categoryService.getCategoryNo(id);
-	        postNo = postService.getPostNo(categoryNo);
+			postNo = postService.getPostNo(categoryNo);
 		}
-		
 
-		BlogVo blogVo = blogService.getBlog(id);
 		List<CategoryVo> listCategory = categoryService.postCount(id);
 		List<PostVo> listPost = postService.getPost(categoryNo);
-		
+
 		PostVo postVoSingle = postService.getPostByNoSingle(postNo);
-		
+
 		model.addAttribute("blogVo", blogVo);
 		model.addAttribute("listCategory", listCategory);
 		model.addAttribute("listPost", listPost);
 		model.addAttribute("postVoSingle", postVoSingle);
 		model.addAttribute("categoryNo", categoryNo);
-		
+
 		return "blog/blog-main";
 	}
-	
+
 	@Auth
 	@RequestMapping("/admin/basic")
 	public String basic(@PathVariable String id, Model model) {
 		BlogVo blogVo = blogService.getBlog(id);
-		
+
 		model.addAttribute("blogVo", blogVo);
-		
+
 		return "blog/blog-admin-basic";
 	}
-	
+
 	@Auth
-	@RequestMapping(value="/admin/basic", method=RequestMethod.POST)
-	public String basic(@PathVariable String id
-			, @RequestParam("title") String title
-			, @RequestParam("logo-file") MultipartFile multipartFile, Model model) {
-		
+	@RequestMapping(value = "/admin/basic", method = RequestMethod.POST)
+	public String basic(@PathVariable String id, @RequestParam("title") String title,
+			@RequestParam("logo-file") MultipartFile multipartFile, Model model) {
+
 		BlogVo blogVo = blogService.getBlog(id);
-		
+
 		String url = blogService.fileUpload(multipartFile);
-		
-		if(url == null) {
+
+		if (url == null) {
 			blogVo = blogService.getBlog(id);
 			url = blogVo.getLogo();
 		}
-		
+
 		blogVo.setTitle(title);
 		blogVo.setLogo(url);
 		blogService.update(blogVo);
-			
+
 		model.addAttribute("blogVo", blogVo);
-		
+
 		return "redirect:/{id}";
 	}
-	
+
 	@Auth
 	@RequestMapping("admin/category")
 	public String category(@PathVariable String id, Model model) {
-		
+
 		BlogVo blogVo = blogService.getBlog(id);
 		List<CategoryVo> categoryVo = categoryService.postCount(id);
-		
+
 		model.addAttribute("blogVo", blogVo);
 		model.addAttribute("categoryVo", categoryVo);
-		
+
 		return "blog/blog-admin-category";
 	}
-	
+
 	@Auth
-	@RequestMapping(value="admin/category", method=RequestMethod.POST)
-	public String category(@PathVariable String id, @RequestParam("name") String name
-			, @RequestParam("desc") String description, Model model) {
-		
+	@RequestMapping(value = "admin/category", method = RequestMethod.POST)
+	public String category(@PathVariable String id, @RequestParam("name") String name,
+			@RequestParam("desc") String description, Model model) {
+
 		Map<String, String> map = new HashMap<>();
-				
+
 		map.put("id", id);
 		map.put("name", name);
 		map.put("description", description);
-		
+
 		categoryService.insert(map);
-			
+
 		return "redirect:/{id}";
 	}
-	
+
 	@Auth
 	@RequestMapping("/admin/delete/{no}")
 	public String delete(@PathVariable String id, @PathVariable String no, Model model) {
 		categoryService.delete(no, id);
 		return "redirect:/{id}";
 	}
-	
+
 	@Auth
 	@RequestMapping("/admin/write")
 	public String write(@PathVariable String id, Model model) {
 		BlogVo blogVo = blogService.getBlog(id);
-		List<CategoryVo> categoryVo = categoryService.postCount(id); 
-		
+		List<CategoryVo> categoryVo = categoryService.postCount(id);
+
 		model.addAttribute("categoryVo", categoryVo);
 		model.addAttribute("blogVo", blogVo);
-		
+
 		return "blog/blog-admin-write";
 	}
-	
+
 	@Auth
-	@RequestMapping(value="/admin/write", method=RequestMethod.POST)
-	public String write(@PathVariable String id
-			, @RequestParam("category") String name
-			, @RequestParam("title") String title, @RequestParam("contents") String contents) {
-		
+	@RequestMapping(value = "/admin/write", method = RequestMethod.POST)
+	public String write(@PathVariable String id, @RequestParam("category") String name,
+			@RequestParam("title") String title, @RequestParam("contents") String contents) {
+
 		Map<String, String> map = new HashMap<>();
 		String no = categoryService.findNoByName(name, id);
-		
+
 		map.put("no", no);
 		map.put("title", title);
 		map.put("contents", contents);
-		
+
 		postService.insert(map);
-		
+
 		return "redirect:/{id}";
 	}
 }
