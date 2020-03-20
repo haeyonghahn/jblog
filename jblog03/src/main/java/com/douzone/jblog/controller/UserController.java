b7ae1b0e8502b2cfe3e7b784f367613ef6bf5adb
User <User@DESKTOP-FQTA9PE>
2020-03-20 17:29:51 +0900

    jblog03 aop, valid, message-converter 설정

--- jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.douzone.jblog.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +22,22 @@ public class UserController {
 	private UserService userService;
 	
 	@RequestMapping("/join")
-	public String join() {
+	public String join(@ModelAttribute UserVo vo) {
 		return "user/join";
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(@ModelAttribute UserVo userVo) {
+	public String join(@ModelAttribute @Valid UserVo userVo, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError e : list) {
+//				System.out.println("ObjectError " + e);
+//			}
+			model.addAllAttributes(result.getModel());
+			return "user/join";
+		}
+		
 		userService.join(userVo);
 		userService.createBlog(userVo);
 		userService.createCategory(userVo);
